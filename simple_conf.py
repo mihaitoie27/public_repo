c7eb4cb6b286db9cd598882c6e7780785465838d
mihaitoie <88823397+mihaitoie@users.noreply.github.com>
2024-10-03 17:35:40 +0300

    clean script

--- simple_conf.py
+++ simple_conf.py
@@ -2,9 +2,5 @@ def print_pattern():
     size = 4
     for i in range(size):
         print("*_*_*_______:)))_4th_next_withHOOK_" * size)
-        print("new print" * size)
-        print("a new branch is created")
-        print("a new branch is created")
-        print("from mainx2")
 
 print_pattern()
