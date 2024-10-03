30dbcd34bfc172a57697f524e48187c1e02fd546
mihaitoie27 <toiemihai27@gmail.com>
2024-10-03 15:26:50 +0300

    change string simple_conf.py
    change string

--- simple_conf.py
+++ simple_conf.py
@@ -1,6 +1,6 @@
 def print_pattern():
     size = 4
     for i in range(size):
-        print("*_*_*_______:)))_4th_next_withHOOK" * size)
+        print("*_*_*_______:)))_4th_next_withHOOK_" * size)
 
 print_pattern()
