cbe3fc8efb508145e66067fee858f407eff1ed16
mihaitoie <88823397+mihaitoie@users.noreply.github.com>
2024-10-03 15:30:50 +0300

    from a new branch

--- simple_conf.py
+++ simple_conf.py
@@ -3,5 +3,6 @@ def print_pattern():
     for i in range(size):
         print("*_*_*_______:)))_4th_next_withHOOK_" * size)
         print("new print" * size)
+        print("a new branch is created")
 
 print_pattern()
